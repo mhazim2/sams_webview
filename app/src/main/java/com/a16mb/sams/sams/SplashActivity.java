213171ed8469d298036592edc408280287a42fdf
Muhammad Hazim <mhazim2@gmail.com>
2017-05-16 15:35:37 +0700

    first commit

---
+++ app/src/main/java/com/a16mb/sams/sams/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.a16mb.sams.sams;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Intent i;
+        i = new Intent(SplashActivity.this, MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
